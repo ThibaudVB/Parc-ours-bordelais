3c5d9894956d2c7411971d58f81984a632f10268
M0-st <matthias.oudart@gmail.com>
2025-01-30 20:29:07 +0100

    delet focntionne

--- views/backend/statuts/delete.php
+++ views/backend/statuts/delete.php
@@ -1,15 +1,16 @@
 <?php
 include '../../../header.php';
 
-if(isset($_GET['numStat'])){
+if (isset($_GET['numStat'])) {
     $numStat = $_GET['numStat'];
     $libStat = sql_select("STATUT", "libStat", "numStat = $numStat")[0]['libStat'];
 }
+
+$fk = sql_select("membre", "numStat", "numStat = $numStat");
+
 ?>
 
-<?php
-$sql "SELECT*FROM"
-?>
+
 
 <!-- Bootstrap form to create a new statut -->
 <div class="container">
@@ -22,13 +23,27 @@ $sql "SELECT*FROM"
             <form action="<?php echo ROOT_URL . '/api/statuts/delete.php' ?>" method="post">
                 <div class="form-group">
                     <label for="libStat">Nom du statut</label>
-                    <input id="numStat" name="numStat" class="form-control" style="display: none" type="text" value="<?php echo($numStat); ?>" readonly="readonly" />
-                    <input id="libStat" name="libStat" class="form-control" type="text" value="<?php echo($libStat); ?>" readonly="readonly" disabled />
+                    <input id="numStat" name="numStat" class="form-control" style="display: none" type="text"
+                        value="<?php echo ($numStat); ?>" readonly="readonly" />
+                    <input id="libStat" name="libStat" class="form-control" type="text"
+                        value="<?php echo ($libStat); ?>" readonly="readonly" disabled />
                 </div>
                 <br />
                 <div class="form-group mt-2">
+                    <?php
+                    if ($sql = "SELECT * FROM table WHERE fk IS NULL") {
+                        $desactiver = true;
+                    }
+                    ?>
+
                     <a href="list.php" class="btn btn-primary">List</a>
-                    <button type="submit" class="btn btn-danger">Confirmer delete ?</button>
+                    <?php 
+                    if (empty($fk)){
+                        echo '<button type="submit" class="btn btn-danger">Confirmer delete ? </button>';
+                    } else {
+                        echo '<button type="submit" class="btn btn-danger" disabled> Annulation impossible</button>';
+                    }
+                    ?>
                 </div>
             </form>
         </div>
