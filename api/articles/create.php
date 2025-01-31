307a90814ec8b7bf95d575c8672c2db2041460d2
M0-st <matthias.oudart@gmail.com>
2025-01-31 15:26:34 +0100

    api test

--- api/articles/create.php
+++ api/articles/create.php
@@ -7,15 +7,15 @@ $dtCreaArt = ctrlSaisies($_POST['dtCreaArt']);
 $libChapoArt = ctrlSaisies($_POST['libChapoArt']);
 $libAccrochArt = ctrlSaisies($_POST['libAccrochArt']);
 $parag1Art = ctrlSaisies($_POST['parag1Art']);
-$libSsTitr1Art = ctrlSaisies($_POST['sousparag1Art']);
+$libSsTitr1Art = ctrlSaisies($_POST['libSsTitr1Art']);
 $parag2Art = ctrlSaisies($_POST['parag2Art']);
-$libSsTitr2Art = ctrlSaisies($_POST['sousparag2Art']);
+$libSsTitr2Art = ctrlSaisies($_POST['libSsTitr2Art']);
 $parag3Art = ctrlSaisies($_POST['parag3Art']);
 $libConclArt = ctrlSaisies($_POST['libConclArt']);
 $urlPhotArt = ctrlSaisies($_POST['urlPhotArt']);
 
-$nom = 
-$values = 
+$nom = "libTitrArt, dtCreaArt, libChapoArt, libAccrochArt, parag1Art, libSsTitr1Art, parag2Art, libSsTitr2Art, parag3Art, libConclArt, urlPhotArt";
+$values = "$libTitrArt, $dtCreaArt, $libChapoArt, $libAccrochArt, $parag1Art, $libSsTitr1Art, $parag2Art, $libSsTitr2Art, $parag3Art, $libConclArt, $urlPhotArt";
 
 sql_insert('ARTICLE', $noms, $values);
 
