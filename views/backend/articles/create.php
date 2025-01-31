80fde33e1b8c62a70e629530252bfa087ee40a89
ThibaudVB <thibaudvbpro@gmail.com>
2025-01-31 15:23:51 +0100

    update variable

--- views/backend/articles/create.php
+++ views/backend/articles/create.php
@@ -33,8 +33,8 @@ if (isset($_GET['libThem'])) {
                 </div>
                 <br />
                 <div class="form-group">
-                    <label for="datecreation">Date de création</label>
-                    <input id="datecreation" name="datecreation" class="form-control" style="resize: vertical;" type="text" autofocus />
+                    <label for="dtCreaArt">Date de création</label>
+                    <input id="dtCreaArt" name="dtCreaArt" class="form-control" style="resize: vertical;" type="text" autofocus />
                 </div>
                 <div class="form-group">
                     <label for="libChapoArt">Chapô de l'article</label>
@@ -51,8 +51,8 @@ if (isset($_GET['libThem'])) {
                     <textarea id="parag1Art" name="parag1Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
                 </div>
                 <div class="form-group">
-                    <label for="sousparag1Art">Sous titre 1</label>
-                    <textarea id="sousparag1Art" name="sousparag1Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
+                    <label for="libSsTitr1Art">Sous titre 1</label>
+                    <textarea id="libSsTitr1Art" name="libSsTitr1Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
                 </div>
                 <br />
                 <div class="form-group">
@@ -60,8 +60,8 @@ if (isset($_GET['libThem'])) {
                     <textarea id="parag2Art" name="parag2Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
                 </div>
                 <div class="form-group">
-                    <label for="sousparag2Art">Sous titre 2</label>
-                    <textarea id="sousparag2Art" name="sousparag1Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
+                    <label for="libSsTitr2Art">Sous titre 2</label>
+                    <textarea id="libSsTitr2Art" name="libSsTitr2Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
                 </div>
                 <br />
                 <div class="form-group">
