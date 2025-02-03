ec3ec2322a0c54f6332d6681f6f81324902b3de0
M0-st <matthias.oudart@gmail.com>
2025-02-03 10:18:24 +0100

    Pb fk create

--- api/articles/create.php
+++ api/articles/create.php
@@ -14,11 +14,11 @@ $parag2Art = ctrlSaisies($_POST['parag2Art']);
 $libSsTitr2Art = ctrlSaisies($_POST['libSsTitr2Art']);
 $parag3Art = ctrlSaisies($_POST['parag3Art']);
 $libConclArt = ctrlSaisies($_POST['libConclArt']);
-$urlPhotArt = ctrlSaisies($_POST['urlPhotArt']);
+$urlPhotArt = $_FILES['urlPhotArt']['name']; 
 
 
 $attributs = "libTitrArt, dtCreaArt, libChapoArt, libAccrochArt, parag1Art, libSsTitr1Art, parag2Art, libSsTitr2Art, parag3Art, libConclArt, urlPhotArt";
-$values = "$libTitrArt, $dtCreaArt, $libChapoArt, $libAccrochArt, $parag1Art, $libSsTitr1Art, $parag2Art, $libSsTitr2Art, $parag3Art, $libConclArt, $urlPhotArt";
+$values = "'$libTitrArt', '$dtCreaArt', '$libChapoArt', '$libAccrochArt', '$parag1Art', '$libSsTitr1Art', '$parag2Art', '$libSsTitr2Art', '$parag3Art', '$libConclArt', '$urlPhotArt'";
 
 sql_insert('ARTICLE', $attributs, $values);
 
--- views/backend/articles/create.php
+++ views/backend/articles/create.php
@@ -26,52 +26,63 @@ if (isset($_GET['libThem'])) {
             <h1>Création d'un article</h1>
         </div>
         <div class="col-md-12">
-            <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post" enctype="multipart/form-data">
+            <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post"
+                enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="libTitrArt">Titre de l'article</label>
-                    <input id="libTitrArt" name="libTitrArt" class="form-control" style="resize: vertical;" type="text" autofocus />
+                    <input id="libTitrArt" name="libTitrArt" class="form-control" style="resize: vertical;" type="text"
+                        autofocus />
                 </div>
                 <br />
                 <div class="form-group">
                     <label for="dtCreaArt">Date de création</label>
-                    <input id="dtCreaArt" name="dtCreaArt" class="form-control" style="resize: vertical;" type="datetime-local" autofocus />
+                    <input id="dtCreaArt" name="dtCreaArt" class="form-control" style="resize: vertical;"
+                        type="datetime-local" autofocus />
                 </div>
                 <div class="form-group">
                     <label for="libChapoArt">Chapô de l'article</label>
-                    <input id="libChapoArt" name="libChapoArt" class="form-control" style="resize: vertical;" type="text" autofocus />
+                    <input id="libChapoArt" name="libChapoArt" class="form-control" style="resize: vertical;"
+                        type="text" autofocus />
                 </div>
                 <br />
                 <div class="form-group">
                     <label for="libAccrochArt">Accroche de l'article</label>
-                    <input id="libAccrochArt" name="libAccrochArt" class="form-control" style="resize: vertical;" type="text" autofocus />
+                    <input id="libAccrochArt" name="libAccrochArt" class="form-control" style="resize: vertical;"
+                        type="text" autofocus />
                 </div>
                 <br />
                 <div class="form-group">
                     <label for="parag1Art">Paragraphe 1</label>
-                    <textarea id="parag1Art" name="parag1Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
+                    <textarea id="parag1Art" name="parag1Art" class="form-control" rows="6"
+                        style="resize: vertical;"></textarea>
                 </div>
                 <div class="form-group">
                     <label for="libSsTitr1Art">Sous titre 1</label>
-                    <textarea id="libSsTitr1Art" name="libSsTitr1Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
+                    <textarea id="libSsTitr1Art" name="libSsTitr1Art" class="form-control" rows="6"
+                        style="resize: vertical;"></textarea>
                 </div>
                 <br />
                 <div class="form-group">
                     <label for="parag2Art">Paragraphe 2</label>
-                    <textarea id="parag2Art" name="parag2Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
+                    <textarea id="parag2Art" name="parag2Art" class="form-control" rows="6"
+                        style="resize: vertical;"></textarea>
                 </div>
                 <div class="form-group">
                     <label for="libSsTitr2Art">Sous titre 2</label>
-                    <textarea id="libSsTitr2Art" name="libSsTitr2Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
+                    <textarea id="libSsTitr2Art" name="libSsTitr2Art" class="form-control" rows="6"
+                        style="resize: vertical;"></textarea>
                 </div>
                 <br />
                 <div class="form-group">
                     <label for="parag3Art">Paragraphe 3</label>
-                    <textarea id="parag3Art" name="parag3Art" class="form-control" rows="6" style="resize: vertical;"></textarea>
+                    <textarea id="parag3Art" name="parag3Art" class="form-control" rows="6"
+                        style="resize: vertical;"></textarea>
                 </div>
                 <br />
                 <div class="form-group">
                     <label for="libConclArt">Conclusion de l'article</label>
-                    <textarea id="libConclArt" name="libConclArt" class="form-control" rows="6" style="resize: vertical;"></textarea>
+                    <textarea id="libConclArt" name="libConclArt" class="form-control" rows="6"
+                        style="resize: vertical;"></textarea>
                 </div>
                 <br />
                 <div class="form-group">
@@ -82,7 +93,7 @@ if (isset($_GET['libThem'])) {
                 <div class="form-group">
                     <label for="numThem">Thématique de l'article</label>
                     <select class="form-select" name="numThem">
-                    <?php
+                        <?php
                         $thematiques = sql_select('thematique', 'numThem, libThem');
                         foreach ($thematiques as $thematique) {
                             echo '<option value="' . $thematique['numThem'] . '">' . $thematique['libThem'] . '</option>';
