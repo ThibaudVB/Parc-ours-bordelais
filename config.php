8a4a890d199a90894353016997cf2ad036c8059f
ThibaudVB <thibaudvbpro@gmail.com>
2025-02-05 12:19:42 +0100

    dépression

--- config.php
+++ config.php
@@ -5,16 +5,16 @@ define('ROOT_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
 //Load env
 require_once ROOT . '/includes/libs/DotEnv.php';
-if(strpos($_SERVER['HTTP_HOST'], 'scalingo')  == false) {
-(new DotEnv(ROOT.'/.env'))->load();
+if(strpos($_SERVER['HTTP_HOST'], 'scalingo') == false) {
+    (new DotEnv(ROOT.'/.env'))->load();
 }
 //defines
 require_once ROOT . '/config/defines.php';
 
 //debug
-if (getenv('APP_DEBUG') == 'true') {
+
     require_once ROOT . '/config/debug.php';
-}
+
 
 //load functions
 require_once ROOT . '/functions/global.inc.php';
